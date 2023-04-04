a4586e64f50dfd59679c8fac735e41a713ce046e
guilherme amorim <cezarguilherme03@gmail.com>
2023-04-04 10:55:57 -0300

    estrutura while

--- Estrutura_Switch_Case/Switch_Case_Menu_Bebidas/Program.cs
+++ Estrutura_Switch_Case/Switch_Case_Menu_Bebidas/Program.cs
@@ -32,98 +32,103 @@ Console.WriteLine(@$"
 ----------------------------------------------------
 ");
 
+bool escolhaBebidaTeste = false;
 int escolhaBebida;
 string escolhaGelo;
 string bebida;
 
-escolhaBebida = int.Parse(Console.ReadLine());
 
+
+while (escolhaBebidaTeste == false)
 {
-switch (escolhaBebida)
-{
-    case 1:
-        bebida = "fanta-uva";
-        Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
-        escolhaGelo = Console.ReadLine().ToLower();
 
-        if (escolhaGelo == "sim")
-        {
-            Console.WriteLine($"ok, sua {bebida} terá gelo");
-        }
-        else
-        {
-            Console.WriteLine($"sua {bebida} é natural");
-        }
-        break;
-    case 2:
-    bebida = "Fanta-guraná";
-        Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
-        escolhaGelo = Console.ReadLine().ToLower();
+    escolhaBebida = int.Parse(Console.ReadLine());
+    switch (escolhaBebida)
+    {
+        case 1:
+            bebida = "fanta-uva";
+            Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
+            escolhaGelo = Console.ReadLine().ToLower();
 
-        if (escolhaGelo == "sim")
-        {
-            Console.WriteLine($"ok, sua {bebida} terá gelo");
-        }
-        else
-        {
-            Console.WriteLine($"sua {bebida}é natural");
-        }
-        break;
-    case 3:
-    bebida = "Sprite";
-        Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
-        escolhaGelo = Console.ReadLine().ToLower();
+            if (escolhaGelo == "sim")
+            {
+                Console.WriteLine($"ok, sua {bebida} terá gelo");
 
-        if (escolhaGelo == "sim")
-        {
-            Console.WriteLine($"ok, sua {bebida} terá gelo");
-        }
-        else
-        {
-            Console.WriteLine($"sua {bebida} é natural");
-        }
-        break;
-    case 4:
-        Console.WriteLine($"Ok, sua escolha de bebida é Coca-cola. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
-        escolhaGelo = Console.ReadLine().ToLower();
+            }
+            else
+            {
+                Console.WriteLine($"sua {bebida} é natural");
+            }
+            break;
+        case 2:
+            bebida = "Fanta-guraná";
+            Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
+            escolhaGelo = Console.ReadLine().ToLower();
 
-        if (escolhaGelo == "sim")
-        {
-            Console.WriteLine($"ok, sua Coca-cola terá gelo");
-        }
-        else
-        {
-            Console.WriteLine($"sua Coca-cola é natural");
-        }
-        break;
-    case 5:
-        Console.WriteLine($"Ok, sua escolha de bebida é Água. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
-        escolhaGelo = Console.ReadLine().ToLower();
+            if (escolhaGelo == "sim")
+            {
+                Console.WriteLine($"ok, sua {bebida} terá gelo");
+            }
+            else
+            {
+                Console.WriteLine($"sua {bebida}é natural");
+            }
+            break;
+        case 3:
+            bebida = "Sprite";
+            Console.WriteLine($"Ok, sua escolha de bebida é {bebida}. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
+            escolhaGelo = Console.ReadLine().ToLower();
 
-        if (escolhaGelo == "sim")
-        {
-            Console.WriteLine($"ok, sua Água terá gelo");
-        }
-        else
-        {
-            Console.WriteLine($"sua Água é natural");
-        }
-        break;
-    default:
-        Console.WriteLine($"Nenhuma opção válida, por gentileza, digite os números corretos referentes às bebidas!");
-        break;
+            if (escolhaGelo == "sim")
+            {
+                Console.WriteLine($"ok, sua {bebida} terá gelo");
+            }
+            else
+            {
+                Console.WriteLine($"sua {bebida} é natural");
+            }
+            break;
+        case 4:
+            Console.WriteLine($"Ok, sua escolha de bebida é Coca-cola. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
+            escolhaGelo = Console.ReadLine().ToLower();
 
+            if (escolhaGelo == "sim")
+            {
+                Console.WriteLine($"ok, sua Coca-cola terá gelo");
+            }
+            else
+            {
+                Console.WriteLine($"sua Coca-cola é natural");
+            }
+            break;
+        case 5:
+            Console.WriteLine($"Ok, sua escolha de bebida é Água. Voce deseja acrescentar gelo ou não ? Digite sim ou não");
+            escolhaGelo = Console.ReadLine().ToLower();
+
+            if (escolhaGelo == "sim")
+            {
+                Console.WriteLine($"ok, sua Água terá gelo");
+            }
+            else
+            {
+                Console.WriteLine($"sua Água é natural");
+            }
+            break;
+        default:
+            Console.WriteLine($"Nenhuma opção válida, por gentileza, digite os números corretos referentes às bebidas!");
+            break;
+
+    }
 }
 
 Console.Clear();
-    
-}
+
 
 // ------------------- Segunda maneira pra criar o prograrama ----------------------
 
 
 //  Console.WriteLine($"voce quer gelo ?");
- 
+
 // escolhaGelo = Console.ReadLine();
 
 // if (escolhaBebida == 1 && escolhaGelo == "sim")
