c7b2e18ead17222884c49e3d4e6f5b6d8baa1946
guilherme amorim <cezarguilherme03@gmail.com>
2023-04-05 10:29:54 -0300

    laço de repetição com do_while

---
+++ Laços-Repetição/Exercicios-laçoRepetição-01/Program.cs
@@ -0,0 +1,130 @@
+﻿//Faça um programa que leia e valide as seguintes informações:
+
+// 1. Nome: diferente de vazio;
+
+// 2. Idade: entre 0 e 100 anos;
+
+// 3. Salário: maior que zero;
+
+// 4. Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a));
+
+//entrada de dados
+// = nome
+// = idade 
+// = salário 
+// = estado civil 
+
+//processamento de dados
+// nome != de 0
+// idade > 0 && idade < 100
+// salário > 0 
+// estado civil == s (solteiro), c (casado), v (viuvo), d (divorciado)
+
+//saída de dados
+//mostrar as saídas de dados validadas;
+
+
+string nome;
+int idade;
+float salario;
+char estadoCivil;
+bool loopBreak = true;
+
+Console.Clear();
+
+Console.WriteLine($"Informe seu nome: ");
+nome = Console.ReadLine();
+
+while (nome == "")
+{
+    Console.WriteLine($"Por gentileza, informe um nome válido!!");
+    nome = Console.ReadLine();
+}
+
+Console.WriteLine($"Informe sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+while (idade < 0 || idade > 100)
+{
+    Console.WriteLine($"Informe uma idade entre 0 e 100");
+    idade = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine($"Informe seu salário:");
+salario = float.Parse(Console.ReadLine());
+
+while (salario < 0)
+{
+    Console.WriteLine($"Informe um salário maior do que 0");
+    salario = float.Parse(Console.ReadLine());
+
+}
+
+do
+{
+    Console.WriteLine(@$"
+Informe seu estado civil:
+'s' -> solteiro
+'c' -> casado
+'d' -> divorciado
+'v' -> viuvo
+ ");
+
+    estadoCivil = char.Parse(Console.ReadLine());
+
+
+    switch (estadoCivil)
+    {
+        case 's':
+            Console.Clear();
+
+            Console.WriteLine($"CARREGANDO...");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine($"O usuário {nome}, com {idade} anos, com a faixa salarial de: {salario} reais, está solteiro");
+            loopBreak = false;
+            break;
+        case 'c':
+            Console.Clear();
+
+            Console.WriteLine($"CARREGANDO...");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine($"O usuário {nome}, com {idade} anos, com a faixa salarial de: {salario} reais, está casado");
+            loopBreak = false;
+            break;
+        case 'd':
+            Console.Clear();
+
+            Console.WriteLine($"CARREGANDO...");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine($"O usuário {nome}, com {idade} anos, com a faixa salarial de: {salario} reais, está divorciado");
+            loopBreak = false;
+            break;
+        case 'v':
+            Console.Clear();
+
+            Console.WriteLine($"CARREGANDO...");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine($"O usuário {nome}, com {idade} anos, com a faixa salarial de: {salario} reais, está viúvo");
+            loopBreak = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine($"Resposta inválida");
+            loopBreak = true;
+            break;
+    }
+
+}
+while (loopBreak);
+
+
+
---
+++ Laços-Repetição/Laços-01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine($"digite seu nome: ");
+string nome = Console.ReadLine();
+
+
+Console.WriteLine($"digite sua senha: ");
+string senha = Console.ReadLine();
+
+while (senha.Length != 6)
+{
+    Console.WriteLine($"Senha digitada não atende aos requisitos! Digite novamente!");
+    senha = Console.ReadLine();
+}
+
+Console.WriteLine($"Senha recebida com sucesso!");
+
