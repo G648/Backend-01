5ed058a55d9c24ebabbd002e6e9ff3e9c53c66b1
guilherme amorim <cezarguilherme03@gmail.com>
2023-05-16 10:55:10 -0300

    projeto cadastro de produtos v1

---
+++ Nova_Materia_OOP/Projeto_CadastroProdutos_16-05/Produto.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_CadastroProdutos_16_05
+{
+    public class Produto
+    {
+        public void CadastrarProduto()
+        {
+
+        }
+
+        public void ListarProduto()
+        {
+
+        }
+
+        public void RemoverProduto()
+        {
+
+        }
+
+        //esses métodos vão cher chamados no login do usuário 
+    } 
+}
