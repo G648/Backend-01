3d7d284e58cf784a4110a20b060911a263509cce
guilherme amorim <cezarguilherme03@gmail.com>
2023-05-31 10:54:01 -0300

    projeto gamer v3

--- Nova_Materia_OOP/Projeto_Gamer_MVC/Controllers/EquipeController.cs
+++ Nova_Materia_OOP/Projeto_Gamer_MVC/Controllers/EquipeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Projeto_Gamer_MVC.Infra;
+using Projeto_Gamer_MVC.Models;
 
 namespace Projeto_Gamer_MVC.Controllers
 {
@@ -24,11 +25,8 @@ namespace Projeto_Gamer_MVC.Controllers
 
         //rota criada para listar as nossas equipes e depois passamos o caminho dessa rota 
         [Route("Listar")] //http://localhost/Equipe/Listar
-
-
         public IActionResult Index()
         {
-
             //criando uma mochila para armazenar as nossas equipes
             //viewbag é responsavel por ter acesso às equipes listadas;
             ViewBag.Equipe = conexaoBancoContext.Equipe.ToList();
@@ -36,6 +34,73 @@ namespace Projeto_Gamer_MVC.Controllers
             //retorna a view de equipe(TELA)
             return View();
         }
+
+        [Route ("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //novo objeto do tipo equipe
+            Equipe novaEquipe = new Equipe();
+
+            //atribuição de valores recebidos do formulário
+            novaEquipe.Nome = form["Nome"].ToString();
+
+            //aqui estava recebendo um valor de imagem em string, porém não é isso que queremos, vamos fazer a lógica para fazer o upload da nossa imagem
+            // novaEquipe.Imagem = form["Imagem"].ToString();
+
+            //Início upload da imagem
+            if (form.Files.Count > 0)
+            {
+                //criando variável para alocar ar imagens desde o índice 0
+                var file = form.Files[0];
+
+                //criando uma varriável para criar o diretório Equipes
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                //gerar o caminho completo até o caminho do arquivo (imagem - nome com extensão)
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroo/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+                
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+
+            //adiciona uma nova tabela no DB
+            conexaoBancoContext.Equipe.Add(novaEquipe);
+
+            //salvar alterações feitas no banco de dados
+            conexaoBancoContext.SaveChanges();
+
+            //retorna para o local chamando a rota de listar
+            return LocalRedirect ("~/Equipe/Listar");
+        }
+
+        // [Route("Deletar")]
+        // public IActionResult Deletar(IFormCollection form)
+        // {
+        //     //instancia de um novo objeto
+        //     Equipe deleteEquipe = new Equipe();
+
+        //     //atribuição de valores
+        //     deleteEquipe.Nome = form["Nome"].ToString();
+
+        //     Equipe equipeEncontrada = ViewBag.Find(x => x.Nome == deleteEquipe.Nome);
+
+        //     return LocalRedirect ("~/Equipe/Listar");
+        // }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
