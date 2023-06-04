c041f61d32194199f61d1eb1616c6070d06b4c06
Guilherme Amorim <cezarguilherme03@gmail.com>
2023-06-04 20:51:59 -0300

    deletando pasta projeto gamer mvc

--- Nova_Materia_OOP/Projeto_Gamer_MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Projeto_Gamer_MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
